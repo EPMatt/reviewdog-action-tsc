15adcb542648f85ac173a036d1df1ffca3cdcfee
Matteo Agnoletto <30753195+EPMatt@users.noreply.github.com>
2021-09-19 17:46:51 +0200

    fix script quoting

--- script.sh
+++ script.sh
@@ -10,29 +10,29 @@ echo "::group::🐶 Installing reviewdog ... https://github.com/reviewdog/review
 curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b "${TEMP_PATH}" "${REVIEWDOG_VERSION}" 2>&1
 echo "::endgroup::"
 
-if [ ! -f "$(npm bin)/tsc" ]; then
-  echo "::group::🔄 Running `npm install` to install tsc ..."
+if [ ! -f "$(npm bin)"/tsc ]; then
+  echo "::group::🔄 Running npm install to install tsc ..."
   npm install
   echo "::endgroup::"
 fi
 
-if [ ! -f "$(npm bin)/tsc" ]; then
+if [ ! -f "$(npm bin)"/tsc ]; then
   echo "❌ Unable to locate or install tsc. Did you provide a workdir which contains a valide package.json?"
   exit 1
 else
 
-  echo "ℹ️ tsc version:"$("$(npm bin)"/tsc --version)""
+  echo ℹ️ tsc version: "$("$(npm bin)"/tsc --version)"
 
   echo "::group::📝 Running tsc with reviewdog 🐶 ..."
 
-  "$(npm bin)"/tsc ${INPUT_TSC_FLAGS:-"."} \
+  "$(npm bin)"/tsc "${INPUT_TSC_FLAGS:-"."}" \
     | reviewdog -f=tsc \
       -name="${INPUT_TOOL_NAME}" \
       -reporter="${INPUT_REPORTER:-github-pr-review}" \
       -filter-mode="${INPUT_FILTER_MODE}" \
       -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
       -level="${INPUT_LEVEL}" \
-      ${INPUT_REVIEWDOG_FLAGS}
+      "${INPUT_REVIEWDOG_FLAGS}"
 
   reviewdog_rc=$?
   echo "::endgroup::"
