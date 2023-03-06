d026aa107487804d907917ba390ed9d1fe686758
Matteo Agnoletto <info@epmatt.com>
2023-03-06 16:51:01 +0100

    Fix action not working on npm v9 (#162)
    * update workflow badges in readme
    
    * use npm root to locate installed tsc version
    
    * Update script.sh
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- script.sh
+++ script.sh
@@ -4,24 +4,24 @@ cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-if [ ! -f "$(npm bin)"/tsc ]; then
+if [ ! -f "$(npm root)"/.bin/tsc ]; then
   echo "::group::🔄 Running npm install to install tsc ..."
   npm install
   echo "::endgroup::"
 fi
 
-if [ ! -f "$(npm bin)"/tsc ]; then
+if [ ! -f "$(npm root)"/.bin/tsc ]; then
   echo "❌ Unable to locate or install tsc. Did you provide a workdir which contains a valid package.json?"
   exit 1
 else
 
-  echo ℹ️ tsc version: "$("$(npm bin)"/tsc --version)"
+  echo ℹ️ tsc version: "$("$(npm root)"/.bin/tsc --version)"
 
   echo "::group::📝 Running tsc with reviewdog 🐶 ..."
 
   # shellcheck disable=SC2086
-  "$(npm bin)"/tsc ${INPUT_TSC_FLAGS} \
-    | reviewdog -f=tsc \
+  "$(npm root)"/.bin/tsc ${INPUT_TSC_FLAGS} |
+    reviewdog -f=tsc \
       -name="${INPUT_TOOL_NAME}" \
       -reporter="${INPUT_REPORTER}" \
       -filter-mode="${INPUT_FILTER_MODE}" \
