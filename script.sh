689443c41b94fb9060b5551abc51a2f29c2b76b5
FI00ds <122728302+FI00ds@users.noreply.github.com>
2025-05-16 23:36:43 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -25,7 +25,7 @@ else
       -name="${INPUT_TOOL_NAME}" \
       -reporter="${INPUT_REPORTER}" \
       -filter-mode="${INPUT_FILTER_MODE}" \
-      -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+      -fail-level="${INPUT_FAIL_LEVEL}" \
       -level="${INPUT_LEVEL}" \
       ${INPUT_REVIEWDOG_FLAGS}
 
